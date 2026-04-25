4176655a2f33063e7c506babc6a73d64b2335405
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-25 02:17:10 -0700

    feat(back/routes): add task routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const tasksRoutes = require('./routes/tasks');
 
 const app = express();
+
+// Middleware.
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} -> ${req.path}`);
+  next();
+});
+
+// Routes.
+app.use('/api/tasks', tasksRoutes);
 
 // Listen to server requests.
 const PORT = process.env.PORT;
