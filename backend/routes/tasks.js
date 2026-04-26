580ab7b32846e364a2037dc1a86bbac1fa7837d3
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-26 13:05:39 -0700

    feat(back/routes): protect tasks routes to prevent unauthorized access

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -5,7 +5,11 @@ const {
   createTask,
   deleteTask
 } = require('../controllers/taskController');
+const requireAuth = require('../middleware/requireAuth');
 const router = express.Router();
+
+// Route protection.
+router.use(requireAuth);
 
 router.get('/', getTasks);
 router.get('/:id', getTask);
