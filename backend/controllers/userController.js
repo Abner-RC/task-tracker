2fe86d46f4515b04906fd99b5cf9ba6cd959256e
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-26 12:02:53 -0700

    feat(back/model): add sign-up static method to handle user creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,15 @@ const logInUser = async (req, res) => {
 
 // Sign-up user.
 const signUpUser = async (req, res) => {
-  res.json({ msg: 'Sign-up user' });
+  const { email, name, departmentId, password } = req.body;
+
+  try {
+    const user = await User.signUp(email, name, departmentId, password);
+
+    res.status(201).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 }
 
 module.exports = {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
@@ -11,10 +12,35 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  departmentId: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
   }
 }, { timestamps: true });
+
+// Sign-up method.
+userSchema.statics.signUp = async function (email, name, departmentId, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('Email already in use');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    email,
+    name,
+    departmentId,
+    password: hash
+  });
+
+  return user;
+}
 
 module.exports = mongoose.model('User', userSchema);
