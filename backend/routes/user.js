d1fa9291bb274490203e7adc598226bb1f4f5336
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-26 10:10:08 -0700

    feat(back/routes): add user routes

---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => { });
+router.get('/:id', (req, res) => { });
+router.post('/login', (req, res) => { });
+router.post('/signup', (req, res) => { });
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const tasksRoutes = require('./routes/tasks');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // Routes.
 app.use('/api/tasks', tasksRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
