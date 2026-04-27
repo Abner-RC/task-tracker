0515a0508269ce2dd55e4fc3a5f1dd33e737da4f
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-26 20:59:21 -0700

    feat(front): protect routes based on user status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import './styles/index.scss';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className='App'>
       <Navbar />
       <div className='pages'>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/sign-up' element={<SignUp />}></Route>
-          <Route path='/log-in' element={<LogIn />}></Route>
+          <Route
+            path='/'
+            element={user ? <Home /> : <Navigate to='/log-in' />}>
+          </Route>
+          <Route
+            path='/sign-up'
+            element={!user ? <SignUp /> : <Navigate to='/' />}>
+          </Route>
+          <Route
+            path='/log-in'
+            element={!user ? <LogIn /> : <Navigate to='/' />}>
+          </Route>
         </Routes>
       </div>
     </div>
