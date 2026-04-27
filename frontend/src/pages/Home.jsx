95eafe20d1635ffdbe48318cbe0a645e46c27ab2
Abner-RC <abnerramirezcastaneda412@gmail.com>
2026-04-26 21:32:00 -0700

    feat(front/pages/home): set up sections layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,28 @@ import '../styles/pages.scss'
 function Home() {
   return (
     <main className='page page--home'>
-      <p>Home page</p>
+      <section className='page--home__section page--home__section--content'>
+        <section className='container-holder'>
+          <h2>Active Tasks</h2>
+          <div className='container'>
+            -
+          </div>
+        </section>
+        <section className='container-holder'>
+          <h2>Users Created</h2>
+          <div className='container'>
+            -
+          </div>
+        </section>
+      </section>
+      <section className='page--home__section page--home__section--form'>
+        <section className='container-holder'>
+          <h2>Create a task</h2>
+          <div className='container'>
+            -
+          </div>
+        </section>
+      </section>
     </main>
   )
 }
